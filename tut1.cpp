dfc3fa85abc25bcdaf03f3831fdaa9712e6f7b35
Bhavi333 <115914175+Bhavi333@users.noreply.github.com>
2022-10-17 13:04:12 +0530

    Add files via upload

---
+++ tut1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"hello bhavi";
+int c = 3;
+float b = 3.36;
+bool we_can = true;
+char bhavi = 's';
+
+cout<<"\nhere the value of c is: "<<c;
+cout<<"\nhere the value of b is: "<<b;
+cout<<"\nhere the value of we_can is: "<<we_can;
+cout<<"\nhere the value of bhavi is: "<<bhavi;
+
+    return 0;
+}
+
+//this slash is for comment
+// is called slash (//)
+/*and this is for 
+many lines 
+like 
+this*/ 
